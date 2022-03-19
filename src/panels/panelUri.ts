5e97452b322c4e405db446b42215e5ec3b9abfb0
Julien Reig <julien.reig6@gmail.com>
2022-03-19 14:07:29 +0100

    fix: missing require dependency from bundling

--- src/panels/panelUri.ts
+++ src/panels/panelUri.ts
@@ -3,7 +3,6 @@ import * as vscode from 'vscode';
 export class PanelUri {
     constructor(
         public readonly toolkit: vscode.Uri,
-        public readonly codicon: vscode.Uri,
         public readonly script: vscode.Uri,
         public readonly style: vscode.Uri
     ) {}
